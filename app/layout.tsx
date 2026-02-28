7de1ae43e524aaa2394dd299e14f0413c45126ea
Simon Olsson <scmolsson@pm.me>
2026-02-28 13:33:00 +0100

    install shadcn and add custom theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${plusJakarta.variable} ${jetBrains_mono.variable} antialiased`}
+        className={`${plusJakarta.variable} ${jetBrains_mono.variable} antialiased dark`}
       >
         {children}
       </body>
